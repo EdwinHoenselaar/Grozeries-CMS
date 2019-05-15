33f6b840f57a8c9ba2818c87749c6862cb052641
Albert <albert@diskus.studio>
2019-05-15 11:55:32 +0200

    add basic file structure;

---
+++ src/components/Sidebar/SidebarContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SidebarContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>this is a sidebar.</h1>
+      </div>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import ReactDOM from 'react-dom';
+import store from './store'
+import React from 'react';
+
+import Sidebar from './components/Sidebar/SidebarContainer'
+import Login from './components/Login/LoginContainer'
+import Store from './components/Store/StoreContainer'
+import Products from './components/Products/ProductsContainer'
+import Orders from './components/Orders/OrdersContainer'
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const rootElement = document.getElementById('root')
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <nav>
+        <Sidebar />
+      </nav>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/store" component={Store} />
+      <Route exact path="/products" component={Products} />
+      <Route exact path="/orders" component={Orders} />
+    </Router>
+  </Provider>,
+  rootElement
+)
